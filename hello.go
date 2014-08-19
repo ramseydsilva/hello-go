2acde603e60e298937de6f33f5a44a3871fea093
Ramsey D'silva <rdsilva@venasolutions.com>
2014-08-19 23:17:03 +0000

    Reverse hello

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "github.com/ramseydsilva/string"
+)
 
 func main() {
-    fmt.Println("Hello, new gopher!")
+    fmt.Println(string.Reverse("Hello, new gopher!"))
 }
